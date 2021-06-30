1395cdc988a3c4b14394a241665ee8ee702fc745
Lucas Shen <lucas.shxn@gmail.com>
2021-06-30 17:31:24 -0400

    minor tweak

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 	user: "postgres",
-	password: "limeRIP5138",
+	password: "password",
 	host: "localhost",
 	port: 5432,
 	database: "perntodo"
