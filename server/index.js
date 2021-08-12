6809bc86993b4cc1cb191ef64a9c74cf5771d43f
Lucas Shen <lucas.shxn@gmail.com>
2021-08-12 13:31:04 -0400

    oopsie

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./database");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //routes
 
@@ -74,6 +80,6 @@ app.delete("/todos/:id", async(req, res) => {
 	}
 })
 
-app.listen(5000, () => {
-	console.log("server started on port 5000");
+app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
 });
